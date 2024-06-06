0e4db569a5de76ab93e8d110640123b400d576c7
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-06-06 00:00:31 -0300

    FEAT - create the user and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should create a valid user' do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
+  it 'should validate a user', focus: true do
+    user = build(:user, email: nil)
+    expect(user).to_not be_valid
+
+    user2 = build(:user, username: nil)
+    expect(user2).to_not be_valid
+  end
 end
